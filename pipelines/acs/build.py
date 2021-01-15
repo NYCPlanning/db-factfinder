a0f14c7fcf7bf6483734e33a33ed92fb53234472
mgraber <mgraber@planning.nyc.gov>
2021-01-15 15:38:32 +0000

    Pass domain to build.py as arg

--- pipelines/acs/build.py
+++ pipelines/acs/build.py
@@ -7,20 +7,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from tqdm import tqdm
 
-# Load .env environmental variables for local runs
-try:
-    env_path = f'{Path(__file__).parent.parent.parent}/.env'
-    load_dotenv(dotenv_path=env_path)
-except:
-    print('.env file is missing ...')
-
-# Initialize pff instance
-pff = Pff(api_key=os.environ['API_KEY'], year = 2018)
-
 # Declare geography and variables involved in this caculation
-geography = ['city', 'tract', 'NTA', 'cd']
-domains = ['demographic','economic','housing','social']
-variables = [(i['pff_variable'], i['domain']) for i in pff.metadata if i['domain'] in domains]
+GEOGS = ['city', 'tract', 'NTA', 'cd']
 
 # Function wrapper calculate for multiprocessing
 def calculate(*args):
@@ -30,10 +18,27 @@ def calculate(*args):
     except:
         print(var, geo, domain)
 
-# Loop through calculations and collect dataframes in dfs
-for args in tqdm(list(itertools.product(variables, geography))):    
-    dfs.append(calculate(args))
+if __name__ == "__main__":
+    # Domain should be one of ['demographic','economic','housing','social']
+    domain = sys.argv[1]
 
-# Concatenate dataframes and export to 1 large csv
-df = pd.concat(dfs)
-df.to_csv('pff_acs.csv', index=False)
+    # Load .env environmental variables for local runs
+    try:
+        env_path = f'{Path(__file__).parent.parent.parent}/.env'
+        load_dotenv(dotenv_path=env_path)
+    except:
+        print('.env file is missing ...')
+
+    # Initialize pff instance
+    pff = Pff(api_key=os.environ['API_KEY'], year = 2018)
+
+    # Create set of variables to loop through
+    variables = [(i['pff_variable'], i['domain']) for i in pff.metadata if i['domain'] == domain]
+
+    # Loop through calculations and collect dataframes in dfs
+    for args in tqdm(list(itertools.product(variables, GEOGS))):    
+        dfs.append(calculate(args))
+    
+    # Concatenate dataframes and export to domain-specific csv
+    df = pd.concat(dfs)
+    df.to_csv(f'pff_acs_{domain}.csv', index=False)
