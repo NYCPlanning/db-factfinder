8c13a4eadb92e0c7c4f3362809c3dbc19a60ce27
mgraber <mgraber@planning.nyc.gov>
2021-05-19 11:49:21 -0400

    Use full geoid in test data

--- tests/test_geography.py
+++ tests/test_geography.py
@@ -22,12 +22,36 @@ def test_2010_to_2020_aggregated_geography():
 
 
 def test_2010_to_2020_ct2010_to_ct2020():
-    assert True
+    AggregatedGeography = importlib.import_module(
+        "factfinder.geography.2010_to_2020"
+    ).AggregatedGeography
+    geography = AggregatedGeography()
+    test_data = {'row_1': ["36005001600", 5825, 398], 'row_2': ["36005001900", 3141, 341]}
+    test_df = pd.DataFrame.from_dict(test_data, orient='index',
+                       columns=['census_geoid', 'e', 'm'])
+    df = geography.ct2010_to_ct2020(test_df)
+    assert df
 
 
 def test_2010_to_2020_tract_to_nta():
+    AggregatedGeography = importlib.import_module(
+        "factfinder.geography.2010_to_2020"
+    ).AggregatedGeography
+    geography = AggregatedGeography()
+    test_data = {'row_1': ["36005001600", 5825, 398], 'row_2': ["36005001900", 3141, 341]}
+    test_df = pd.DataFrame.from_dict(test_data, orient='index',
+                       columns=['census_geoid', 'e', 'm'])
+    df = geography.tract_to_nta(test_df)
     assert True
 
 
 def test_2010_to_2020_tract_to_cdta():
+    AggregatedGeography = importlib.import_module(
+        "factfinder.geography.2010_to_2020"
+    ).AggregatedGeography
+    geography = AggregatedGeography()
+    test_data = {'row_1': ["36005001600", 5825, 398], 'row_2': ["36005001900", 3141, 341]}
+    test_df = pd.DataFrame.from_dict(test_data, orient='index',
+                       columns=['census_geoid', 'e', 'm'])
+    df = geography.tract_to_cdta(test_df)
     assert True
