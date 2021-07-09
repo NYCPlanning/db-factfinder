7e8a09888f361bd66aefee8097fe6d63f16dde3c
sptkl <caobaiyue@gmail.com>
2021-07-09 15:35:34 -0400

    update acs pipeline to include boro and city

---
+++ pipelines/__init__.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ["API_KEY"]
+ROOT_PATH = Path(__file__).parent.parent
+BASE_PATH = ROOT_PATH / ".output"
+
+if not os.path.isdir(BASE_PATH):
+    os.makedirs(BASE_PATH, exist_ok=True)
+    with open(BASE_PATH / ".gitignore", "w") as f:
+        f.write("*")
+    # os.makedirs(BASE_PATH/"datasets", exist_ok=True)
+    # os.makedirs(BASE_PATH/"configurations", exist_ok=True)
