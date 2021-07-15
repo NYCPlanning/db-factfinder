722f71dbfcd57815ead88dcd69ef27cfa5118dac
mgraber <mgraber@planning.nyc.gov>
2021-07-15 15:03:21 +0000

    Remove int in substraction step

--- factfinder/utils.py
+++ factfinder/utils.py
@@ -57,12 +57,12 @@ def get_median(ranges, row):
     i = i - 1
     if i == 0:
         median = list(ranges.values())[0][1]
-    elif C == 0:
-        median = 0
+    elif C == 0.0:
+        median = 0.0
     elif i == len(ranges.keys()) - 1:
         median = list(ranges.values())[-1][0]
     else:
-        C = C - int(row[ordered[i]])
+        C = C - row[ordered[i]]
         L = ranges[ordered[i]][0]
         F = int(row[ordered[i]])
         W = ranges[ordered[i]][1] - ranges[ordered[i]][0]
