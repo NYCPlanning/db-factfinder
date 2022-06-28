4197e195ce8902b86c10ef81d6ccf0f64904d13b
Andrew Brieff <abreff@gmail.com>
2022-06-28 16:30:46 -0400

    fixes

--- pipelines/acs16_20.py
+++ pipelines/acs16_20.py
@@ -1,16 +1,19 @@
 import argparse
 import os
 import sys
+from typing import Tuple
 
 import pandas as pd
 import re
 
-def parse_args() -> str:
+def parse_args() -> Tuple[str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type=str, help="Location of input ACS file")
-
+    parser.add_argument(
+        "-y", "--year", type=str, help="The ACS5 year, e.g. 2019 (2014-2018)"
+    )
     args = parser.parse_args()
-    return args.input
+    return args.input, args.year
 
 def transform_dataframe(df, domain):
     pff_field_names = extract_field_names(df)
@@ -22,11 +25,8 @@ def transform_dataframe(df, domain):
         new_df['domain'] = domain
         if output_df.empty:
             output_df = new_df
-        elif set(new_df.columns) == set(output_df.columns):
-            output_df = pd.concat([output_df, new_df], ignore_index=True)
         else:
-            print(field_name)
-            print(domain)
+            output_df = pd.concat([output_df, new_df], ignore_index=True)
     return output_df
 
 def extract_field_names(df):
@@ -37,27 +37,21 @@ def split_by_field_name(df, pff_field_name):
 
 if __name__ == "__main__":
     # Get ACS year
-    input = parse_args()
+    input_file, year = parse_args()
 
-    excel_file = pd.ExcelFile("ACS1620Data_06-23-22.xlsx")
-    data_frames = pd.read_excel(excel_file, sheet_name=['Dem1620', 'Social1620', 'Econ1620', 'Housing1620'])
-
-    domains = {
-        "Dem1620": "demographic",
-        "Social1620": "social",
-        "Econ1620": "economic",
-        "Housing1620": "housing"
-    }
+    excel_file = pd.ExcelFile(input_file)
+    data_frames = pd.read_excel(excel_file, sheet_name=[0, 1, 2, 3])
+    domains = ['demographic', 'social', 'economic', 'housing']
 
     export_df = pd.DataFrame()
-    for sheet_name, domain in domains.items():
-        export_df = pd.concat([export_df, transform_dataframe(data_frames[sheet_name], domain)])
+    for idx, domain in enumerate(domains):
+        export_df = pd.concat([export_df, transform_dataframe(data_frames[idx], domain)])
 
     export_df.rename(columns={"geotype": "labs_geotype", "geoid": "labs_geoid"}, inplace=True)
     export_df = export_df[['labs_geotype', 'labs_geoid', 'pff_variable', 'e', 'm', 'c', 'p', 'z', 'domain']]
 
     # Concatenate dataframes and export to 1 large csv
-    output_folder = f".output"
+    output_folder = f".output/{ year }"
     os.makedirs(output_folder, exist_ok=True)
     export_df.to_csv(f"{output_folder}/acs.csv", index=False)
 
