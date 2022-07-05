7cec7ed87ac9f59678496e0f5dbf74a03739078d
Andrew Brieff <abrieff@gmail.com>
2022-07-05 11:36:21 -0400

    PFF - Base Variable Mapping (#231)
    1. Add in function to map PFF Variable from Script to Base Variable from Metadata.json file
    2. Refactor Script structure to be more modular, explicit.
    3. Add in checks for correct year
    4. Remove null rows and extra columns
    
    * Add in column mapping under pff variable assumption
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>

--- pipelines/acs_2020_manual_update.py
+++ pipelines/acs_2020_manual_update.py
@@ -5,54 +5,112 @@ from typing import Tuple
 import pandas as pd
 import re
 
-def parse_args() -> Tuple[str, str, str]:
+OUTPUT_SCHEMA_COLUMNS = ['labs_geotype', 'labs_geoid', 'pff_variable', 'base_variable', 'e', 'm', 'c', 'p', 'z', 'domain']
+
+def parse_args() -> Tuple[str, str]:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type=str, help="Location of input ACS file")
+
     parser.add_argument(
-        "-y", "--year", type=str, help="The ACS5 year, e.g. 2019 (2014-2018)"
+        "-y", "--year", type=str, help="The ACS5 year, e.g. 2020 (2016-2020)", choices=['2010', '2020']
     )
     parser.add_argument(
         "-g", "--geography", type=str, help="The geography year, e.g. 2010_to_2020"
     )
     args = parser.parse_args()
-    return args.input, args.year, args.geography
+    return args.year, args.geography
+
+def pivot_field_name(df, field_name, domain):
+    field_name_df = split_by_field_name(df, field_name)
+
+    field_name_df.rename(columns=lambda column_name: re.sub(f"^{ field_name }(E|M|C|P|Z)$",r"\1", column_name).lower(), inplace=True)
+    field_name_df['pff_variable'] = field_name.lower()
+    field_name_df['domain'] = domain
+
+    return field_name_df
+
+def extract_field_names(df):
+    return df.columns.drop(['GeoType', 'GeoID']).str[:-1].drop_duplicates()
+     
+def split_by_field_name(df, pff_field_name):
+    return df.filter(regex=f"^(GeoType|GeoID|{ pff_field_name }(E|M|C|P|Z))$")
+
+def strip_unnamed_columns(df):
+    return df.loc[:,~df.columns.str.match("Unnamed")]
+
+def sheet_names(year):
+    if year == '2010':
+        sheet_name_suffix = '0610'
+        inflated = "_Inflated"
+    if year == '2020':
+        sheet_name_suffix = '1620'
+        inflated = ""
+
+    domains_sheets = [
+        {
+            'domain': 'demographic',
+            'sheet_name': f'Dem{sheet_name_suffix}'
+        }, 
+        {
+            'domain': 'social',
+            'sheet_name': f'Social{sheet_name_suffix}'
+        }, 
+        {
+            'domain': 'economic',
+            'sheet_name': f'Econ{sheet_name_suffix}{inflated}'
+        }, 
+        {
+            'domain': 'housing',
+            'sheet_name': f'Housing{sheet_name_suffix}{inflated}'
+        }
+    ]
+    return domains_sheets
 
 def transform_dataframe(df, domain):
-    base_field_names = extract_field_names(df)
+    df = strip_unnamed_columns(df)
+    pff_field_names = extract_field_names(df)
     output_df = pd.DataFrame()
 
-    for field_name in base_field_names:
-        new_df = split_by_field_name(df, field_name)
-        new_df = new_df.rename(columns=lambda x: re.sub(f"^{ field_name }(E|M|C|P|Z)$",r"\1",x).lower())
-        new_df['base_variable'] = field_name.lower()
-        new_df['domain'] = domain
-        
+    for field_name in pff_field_names:
+
+        new_df = pivot_field_name(df, field_name, domain)
+
         if output_df.empty:
             output_df = new_df
         else:
             output_df = pd.concat([output_df, new_df], ignore_index=True)
-
     return output_df
 
-def extract_field_names(df):
-    return df.columns[2:].str[:-1].drop_duplicates()
-     
-def split_by_field_name(df, base_field_name):
-    return df.filter(regex=f"^(GeoType|GeoID|{ base_field_name }(E|M|C|P|Z))$")
+def transform_all_dataframes(year):
+    domains_sheets = sheet_names(year)
+    input_file = f"factfinder/data/acs_1620_update/{year}/acs_{year}.xlsx"
+    
+    dfs = pd.read_excel(input_file, sheet_name=None, engine='openpyxl')
+    combined_df = pd.DataFrame()
+
+    for domain_sheet in domains_sheets:
+        combined_df = pd.concat([combined_df, transform_dataframe(dfs[domain_sheet['sheet_name']], domain_sheet['domain'])])
+
+    combined_df.dropna(subset=['geotype'], inplace=True)
+
+    return combined_df
+
+def attach_base_variable(df, year):
+    metadata_file = f"factfinder/data/acs/{year}/metadata.json"
+    acs_variable_mapping = pd.read_json(metadata_file)[['base_variable', 'pff_variable']]
+
+    return df.merge(acs_variable_mapping, how='left', on='pff_variable')
+
+def rename_columns(df):
+    df.rename(columns={"geotype": "labs_geotype", "geoid": "labs_geoid"}, inplace=True)
+    return df[OUTPUT_SCHEMA_COLUMNS]
 
 if __name__ == "__main__":
     # Get ACS year
-    input_file, year, geography = parse_args()
+    year, geography = parse_args()
 
-    data_frames = pd.read_excel(input_file, sheet_name=[0, 1, 2, 3], engine='openpyxl')
-    domains = ['demographic', 'social', 'economic', 'housing']
-    export_df = pd.DataFrame()
-
-    for idx, domain in enumerate(domains):
-        export_df = pd.concat([export_df, transform_dataframe(data_frames[idx], domain)])
-
-    export_df.rename(columns={"geotype": "labs_geotype", "geoid": "labs_geoid"}, inplace=True)
-    export_df = export_df[['labs_geotype', 'labs_geoid', 'base_variable', 'e', 'm', 'c', 'p', 'z', 'domain']]
+    export_df = transform_all_dataframes(year)
+    export_df = attach_base_variable(export_df, year)
+    export_df = rename_columns(export_df)
 
     output_folder = f".output/acs/year={year}/geography={geography}"
 
