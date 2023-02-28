6a67a8e67e8a8c19149fa7ebea0d20ba41a29f6f
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-28 16:03:57 -0500

    formatting and logging

--- pipelines/acs_2020_manual_update.py
+++ pipelines/acs_2020_manual_update.py
@@ -31,7 +31,10 @@ def parse_args() -> Tuple[str, str]:
         choices=["2010", "2020"],
     )
     parser.add_argument(
-        "-g", "--geography", type=str, help="The geography year, e.g. 2010_to_2020"
+        "-g",
+        "--geography",
+        type=str,
+        help="The geography year, e.g. 2010_to_2020",
     )
     args = parser.parse_args()
     return args.year, args.geography
@@ -122,16 +125,13 @@ def transform_all_dataframes(year):
 
 def filter_by_metadata(df, year):
     metadata_file = f"factfinder/data/acs/{year}/metadata.json"
-    acs_variable_mapping = pd.read_json(metadata_file)[
-        ["pff_variable"]
-    ]
+    acs_variable_mapping = pd.read_json(metadata_file)[["pff_variable"]]
 
     return df.merge(acs_variable_mapping, how="inner", on="pff_variable")
 
 
 def rename_columns(df):
-    df.rename(columns={"geotype": "labs_geotype",
-              "geoid": "labs_geoid"}, inplace=True)
+    df.rename(columns={"geotype": "labs_geotype", "geoid": "labs_geoid"}, inplace=True)
     return df.reindex(columns=OUTPUT_SCHEMA_COLUMNS)
 
 
@@ -139,11 +139,17 @@ if __name__ == "__main__":
     # Get ACS year
     year, geography = parse_args()
 
+    print("transform_all_dataframes ...")
     export_df = transform_all_dataframes(year)
+
+    print("filter_by_metadata ...")
     export_df = filter_by_metadata(export_df, year)
+
+    print("rename_columns ...")
     export_df = rename_columns(export_df)
 
     output_folder = f".output/acs/year={year}/geography={geography}"
 
+    print("export_df.to_csv ...")
     os.makedirs(output_folder, exist_ok=True)
     export_df.to_csv(f"{output_folder}/acs_manual_update.csv", index=False)
