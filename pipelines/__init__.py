751204ba45477f00700ae76890fc2a01bf48e3cb
Damon McCullough <dmccullough@planning.nyc.gov>
2023-03-06 14:51:42 -0500

    update 0610 and 1721 ACS data for 2022 build (#252)
    * adding two more variables, only upload to s3 if we are on dev branch
    
    * Create PFF variable lookup table for 2020
    
    * fix
    
    * delete unnecessary files
    
    * #222: Excel to Output Schema Transformation Script (#226)
    
    Adds script for producing acs csv based on input excel tables
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * fix
    
    * PFF - Base Variable Mapping (#231)
    
    1. Add in function to map PFF Variable from Script to Base Variable from Metadata.json file
    2. Refactor Script structure to be more modular, explicit.
    3. Add in checks for correct year
    4. Remove null rows and extra columns
    
    * Add in column mapping under pff variable assumption
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * Renamed and added null census columns (#235)
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * use non-inflated numbers from population
    
    * 232 upload ase output manual update to do (#233)
    
    * Add github action for uploading manual update to DO
    
    * try adding auto trigger
    
    * Fix branch with api key
    
    * Fix branch with api key
    
    * switch to manual runs
    
    * rename to match correct on do
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * Update just the metadata files on dev from the Aug Branch (#239)
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * Remove base variable from CSV (#241)
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * Update 2020 Metadata to add Base Variable for grpi20t24 (#245)
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * Remove ACS Variables from 2010 that don't match metadata file (#244)
    
    * Revert "Remove base variable from CSV (#241)"
    
    This reverts commit ef50b5bbdda619bdf0be5884f1b5f981bb753021.
    
    * remove base variable, match on metadata
    
    * dont include base variable unnecessarily
    
    * rename function
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * Updated 2020 data (#247)
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * updated 2020 data (#248)
    
    Co-authored-by: Andrew Brieff <abreff@gmail.com>
    
    * add example.env
    
    * drop poetry and simplify devcontainer
    
    * ignore cached data folder
    
    * add openpyxl python package
    
    * formatting and logging
    
    * fix metadata test
    
    * handle 2021 update
    
    * gitignore
    
    * force action run
    
    * force action run
    
    * generalize manual update
    
    * drop push trigger
    
    * use requirements.txt in action
    
    * replace 2010 acs manual update data
    
    * cleanup inits
    
    * cleanup
    
    * use excel sheets with inflated data for 2010
    
    * add note about excel sheet suffix for inflated data
    
    * add description to script
    
    ---------
    
    Co-authored-by: sptkl <caobaiyue@gmail.com>
    Co-authored-by: Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
    Co-authored-by: Jingyi Huang <jh5721@nyu.edu>
    Co-authored-by: Andrew Brieff <abrieff@gmail.com>
    Co-authored-by: Jingyi Huang <44253950+Oysters1874@users.noreply.github.com>
    Co-authored-by: Andrew Brieff <abreff@gmail.com>

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -11,7 +11,3 @@ BASE_PATH = ROOT_PATH / ".output"
 
 if not os.path.isdir(BASE_PATH):
     os.makedirs(BASE_PATH, exist_ok=True)
-    with open(BASE_PATH / ".gitignore", "w") as f:
-        f.write("*")
-    # os.makedirs(BASE_PATH/"datasets", exist_ok=True)
-    # os.makedirs(BASE_PATH/"configurations", exist_ok=True)
